42f03b74dd37e1b323d92b5dd8d03bd108206189
JK117 <sheppard.jk@outlook.com>
2020-03-31 18:45:46 -0400

    Initiate Pi-Displayer

---
+++ TestOled.py
@@ -0,0 +1,18 @@
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+import time
+
+# rev.1 users set port=0
+# substitute spi(device=0, port=0) below if using that interface
+# serial = i2c(port=1, address=0x3C)
+serial = spi(device=0, port=0)
+
+# substitute ssd1331(...) or sh1106(...) below if using that device
+# 这里改ssd1306, ssd1325, ssd1331, sh1106
+device = sh1106(serial)
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, fill="black", outline="white")
+    draw.text((30, 40), "Hello World", fill="white")
+time.sleep(10)
