922432774c58e007c3ac964dbf37fddfc2b4f750
JK117 <sheppard.jk@outlook.com>
2020-09-25 12:26:30 -0400

    Update cpu test module

--- cpu_1.py
+++ cpu_1.py
@@ -1,9 +1,15 @@
 import os
 
 
+# Return CPU temperature as a character string
 def get_cpu_temperature():
     res = os.popen('vcgencmd measure_temp').readline()
-    return res.replace("temp=", "").replace("'C\n", "")
+    return res.replace("temp=", "")
+
+
+# Return % of CPU used by user as a character string
+def get_cpu_use():
+    return str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip())
 
 
 if __name__ == '__main__':
