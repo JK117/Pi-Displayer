9a10bfb056bfee185b27a4d26e5cb3303dc27e49
JK117 <sheppard.jk@outlook.com>
2020-10-01 16:04:10 -0400

    Update start_up module with cpu info display

--- start_up.py
+++ start_up.py
@@ -2,17 +2,33 @@ from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 import time
+import status
 
-# rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-# serial = i2c(port=1, address=0x3C)
-serial = spi(device=0, port=0, cs_high=True)
 
-# substitute with ssd1306, ssd1325, ssd1331, sh1106
-device = sh1106(serial)
+def main():
+    cpu_temp = status.get_cpu_temp()
+    cpu_usage = status.get_cpu_usage()
+    ram_info = status.get_ram_info()
+    disk_usage = status.get_disk_usage()
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, fill="black", outline="white")
+        draw.text((10, 10), "Raspberry Pi", fill="white")
+        draw.text((10, 30), "Start Up Completed", fill="white")
+    time.sleep(15)
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, fill="white")
+    time.sleep(5)
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, fill="black", outline="white")
+        draw.text((10, 10), cpu_temp, fill="white")
+        draw.text((10, 30), cpu_usage, fill="white")
+    time.sleep(15)
 
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, fill="black", outline="white")
-    draw.text((10, 10), "Raspberry Pi", fill="white")
-    draw.text((10, 30), "Start Up Completed", fill="white")
-time.sleep(15)
+
+if __name__ == "__main__":
+    try:
+        serial = spi(device=0, port=0, cs_high=True)
+        device = sh1106(serial)
+        main()
+    except KeyboardInterrupt:
+        pass
