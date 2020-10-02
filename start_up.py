8ddd068a2ac9b4bb59558ec2d5e7e8e96131541d
JK117 <sheppard.jk@outlook.com>
2020-10-01 23:26:41 -0400

    Update output in start_up module

--- start_up.py
+++ start_up.py
@@ -14,18 +14,18 @@ def main():
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, fill="white")
     time.sleep(5)
-    while True:
-        cpu_temp = status.get_cpu_temp()
-        cpu_usage = status.get_cpu_usage()
-        # ram_info = status.get_ram_info()
-        # disk_usage = status.get_disk_info()
-        with canvas(device) as draw:
-            draw.rectangle(device.bounding_box, fill="black", outline="white")
-            draw.text((10, 5), "CPU Temperature: ", fill="white")
-            draw.text((10, 15), cpu_temp + "'C", fill="white")
-            draw.text((10, 25), "CPU Usage: ", fill="white")
-            draw.text((10, 35), cpu_usage + "%", fill="white")
-        time.sleep(1)
+    # while True:
+    #     cpu_temp = status.get_cpu_temp()
+    #     cpu_usage = status.get_cpu_usage()
+    #     # ram_info = status.get_ram_info()
+    #     # disk_usage = status.get_disk_info()
+    #     with canvas(device) as draw:
+    #         draw.rectangle(device.bounding_box, fill="black", outline="white")
+    #         draw.text((10, 5), "CPU Temperature: ", fill="white")
+    #         draw.text((10, 15), cpu_temp + "'C", fill="white")
+    #         draw.text((10, 25), "CPU Usage: ", fill="white")
+    #         draw.text((10, 35), cpu_usage + "%", fill="white")
+    #     time.sleep(1)
 
 
 if __name__ == "__main__":
