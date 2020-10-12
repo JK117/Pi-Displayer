2ea3aa29bf03e20d39c586f1aec6a1ddcca84047
JK117 <sheppard.jk@outlook.com>
2020-10-12 03:28:04 -0400

    Update test font module to compare pixel fonts

--- test_font.py
+++ test_font.py
@@ -62,11 +62,39 @@ def main():
         "miscfs_.ttf"
     ]
 
-    for i in range(len(pixel_font_list)):
-        font = ImageFont.truetype(pixel_font_list[i], 10)
+    # for i in range(len(pixel_font_list)):
+    #     font = ImageFont.truetype(pixel_font_list[i], 10)
+    #     with canvas(device) as draw:
+    #         draw.rectangle(device.bounding_box, fill="black", outline="white")
+    #         draw.text((10, 10), str(i) + ". This is " + pixel_font_list[i], fill="white", font=font)
+    #     time.sleep(2)
+    font_0 = ImageFont.truetype(pixel_font_list[0], 10)
+    font_1 = ImageFont.truetype(pixel_font_list[1], 10)
+    font_2 = ImageFont.truetype(pixel_font_list[2], 10)
+    font_3 = ImageFont.truetype(pixel_font_list[3], 10)
+    font_4 = ImageFont.truetype(pixel_font_list[4], 10)
+    font_5 = ImageFont.truetype(pixel_font_list[5], 10)
+    font_6 = ImageFont.truetype(pixel_font_list[6], 10)
+    font_7 = ImageFont.truetype(pixel_font_list[7], 10)
+    font_8 = ImageFont.truetype(pixel_font_list[8], 10)
+    font_9 = ImageFont.truetype(pixel_font_list[9], 10)
+
+    while True:
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, fill="black", outline="white")
-            draw.text((10, 10), str(i) + ". This is " + pixel_font_list[i], fill="white", font=font)
+            draw.text((10, 5), pixel_font_list[0], fill="white", font=font_0)
+            draw.text((10, 15), pixel_font_list[1], fill="white", font=font_1)
+            draw.text((10, 25), pixel_font_list[2], fill="white", font=font_2)
+            draw.text((10, 35), pixel_font_list[3], fill="white", font=font_3)
+            draw.text((10, 45), pixel_font_list[4], fill="white", font=font_4)
+        time.sleep(2)
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, fill="black", outline="white")
+            draw.text((10, 5), pixel_font_list[5], fill="white", font=font_5)
+            draw.text((10, 15), pixel_font_list[6], fill="white", font=font_6)
+            draw.text((10, 25), pixel_font_list[7], fill="white", font=font_7)
+            draw.text((10, 35), pixel_font_list[8], fill="white", font=font_8)
+            draw.text((10, 45), pixel_font_list[9], fill="white", font=font_9)
         time.sleep(2)
 
 
