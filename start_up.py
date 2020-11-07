30c961b933963957c4f80482f6c8f47be7b24329
JK117 <sheppard.jk@outlook.com>
2020-11-07 16:35:22 -0500

    Add clock function

--- start_up.py
+++ start_up.py
@@ -1,44 +1,60 @@
 from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import sh1106
+
 from PIL import ImageFont
+
 import time
+import datetime
 import status
 
 
-def main():
+def start_up(device):
     with canvas(device) as draw:
-        free_pixel = ImageFont.truetype("FreePixel.ttf", 12)
-        code_2000 = ImageFont.truetype("code2000.ttf", 12)
         draw.rectangle(device.bounding_box, fill="black", outline="white")
         draw.text((10, 5), "Raspberry Pi", fill="white")
-        # draw.text((10, 15), "Start Up Completed", fill="white")
-        draw.text((10, 15), "Raspberry Pi", fill="white", font=free_pixel)
-        draw.text((10, 30), "Raspberry Pi", fill="white", font=code_2000)
-    time.sleep(20)
-    # for i in range(5):
-    #     with canvas(device) as draw:
-    #         draw.rectangle(device.bounding_box, fill="white")
-    #     time.sleep(10)
+        draw.text((10, 15), "Start Up Completed", fill="white")
+    time.sleep(5)
 
-    # while True:
-    #     cpu_temp = status.get_cpu_temp()
-    #     cpu_usage = status.get_cpu_usage()
-    #     # ram_info = status.get_ram_info()
-    #     # disk_usage = status.get_disk_info()
-    #     with canvas(device) as draw:
-    #         draw.rectangle(device.bounding_box, fill="black", outline="white")
-    #         draw.text((10, 5), "CPU Temperature: ", fill="white")
-    #         draw.text((10, 15), cpu_temp + "'C", fill="white")
-    #         draw.text((10, 25), "CPU Usage: ", fill="white")
-    #         draw.text((10, 35), cpu_usage + "%", fill="white")
-    #     time.sleep(1)
+
+def display_time(device):
+    for i in range(300):
+        with canvas(device) as draw:
+            dt = datetime.datetime.now()
+            dt_date = dt.strftime('%Y-%m-%d')
+            dt_time = dt.strftime('%H:%M:%S')
+            dt_day = dt.strftime('%a')
+            draw.rectangle(device.bounding_box, fill="black", outline="white")
+            draw.text((10, 5), dt_date, fill="white")
+            draw.text((10, 15), dt_time, fill="white")
+            draw.text((10, 25), dt_day, fill="white")
+        time.sleep(1)
+
+
+def status_info(device):
+    while True:
+        cpu_temp = status.get_cpu_temp()
+        cpu_usage = status.get_cpu_usage()
+        # ram_info = status.get_ram_info()
+        # disk_usage = status.get_disk_info()
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, fill="black", outline="white")
+            draw.text((10, 5), "CPU Temperature: ", fill="white")
+            draw.text((10, 15), cpu_temp + "'C", fill="white")
+            draw.text((10, 25), "CPU Usage: ", fill="white")
+            draw.text((10, 35), cpu_usage + "%", fill="white")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
     try:
         serial = spi(device=0, port=0, cs_high=True)
-        device = sh1106(serial)
-        main()
+        spi_device = sh1106(serial)
+        start_up(spi_device)
+        display_time(spi_device)
     except KeyboardInterrupt:
         pass
+
+    # while True:
+
+
