7967902cec16011b6fe11795f6e0a82ad341cf0e
JK117 <sheppard.jk@outlook.com>
2020-11-22 04:52:49 -0500

    Updated start_up module to test text rendering position

--- start_up.py
+++ start_up.py
@@ -19,10 +19,10 @@ def start_up(device):
 
 def display_time(device):
     # font_large = ImageFont.truetype("Quicksand-Bold.ttf", 25)
-    font_small = ImageFont.truetype("Quicksand-Bold.ttf", 15)
+    # font_small = ImageFont.truetype("Quicksand-Bold.ttf", 15)
 
-    font_large = ImageFont.truetype("DejaVuSansMono.ttf", 22)
-    # font_small = ImageFont.truetype("DejaVuSansMono.ttf", 12)
+    font_large = ImageFont.truetype("DejaVuSansMono-Bold.ttf", 22)
+    font_small = ImageFont.truetype("DejaVuSansMono-Bold.ttf", 12)
 
     h = device.height
     w = device.width
