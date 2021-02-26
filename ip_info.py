00cf6864283a414068ba95a32deb6ceb784d70be
JK117 <sheppard.jk@outlook.com>
2021-02-26 22:06:10 +0800

    Separate  public ip acquire connection inside/outside China to achieve better network performance.

--- ip_info.py
+++ ip_info.py
@@ -14,9 +14,23 @@ def get_local_ip():
     return ip
 
 
-def get_public_ip(url='https://ipinfo.io/json?token=592622f207ab66'):
+# For better connection inside US
+def get_public_ip_us(url='https://ipinfo.io/json?token=592622f207ab66'):
     ip = json.load(urlopen(url))['ip']
     return ip
+
+
+# For better connection inside CN
+def get_public_ip_cn(url='http://ip.cip.cc/'):
+    ip = urlopen(url).read().decode('utf-8')
+    return ip
+
+
+# Bad connection inside CN
+def get_public_ip_detail(url='https://freegeoip.app/json/'):
+    ip_detail = json.load(urlopen(url))
+    print(ip_detail)
+    # return ip
 
 
 # def fetch_data(url):
@@ -34,7 +48,7 @@ if __name__ == "__main__":
     try:
         hostname = get_hostname()
         local_ip = get_local_ip()
-        public_ip = get_public_ip()
+        public_ip = get_public_ip_cn()
     except IOError:
         print("Error: unable to get ips")
     else:
